e192d2c21fd616bfbe574bdf682cd6e049cb0ab6
Brad Stone <brad.stone@re-leased.com>
2022-05-18 15:28:17 +1200

    Update build 18-05-22.3

--- dist/js/home.e17f8dee.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmw_vue"]=self["webpackChunkmw_vue"]||[]).push([[177],{3151:function(e,a,n){n.r(a),n.d(a,{default:function(){return d}});var t=n(3396);function u(e,a,n,u,r,c){const o=(0,t.up)("Tabs");return(0,t.wg)(),(0,t.j4)(o,{active:"home"})}var r=n(678),c=n(240),o=n(8871),s=(0,t.aZ)({name:"HomeView",components:{Tabs:c.Z,HeaderBar:o.Z},computed:{isHomePage:()=>"home"==(0,r.tv)().currentRoute.value.name}}),m=n(89);const v=(0,m.Z)(s,[["render",u],["__scopeId","data-v-bc4a9494"]]);var d=v}}]);
-//# sourceMappingURL=home.e17f8dee.js.map
