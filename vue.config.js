d106e783839cf3968cae0539ea912652111e9b2a
Brad Stone <brad.stone@re-leased.com>
2022-05-24 14:05:23 +1200

    Update public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/mw_vue/' : '/',
+  publicPath: '/',
   transpileDependencies: true,
 
   pluginOptions: {
